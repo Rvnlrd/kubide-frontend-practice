48607cff643cf20f23c5ac792d3e645cfb11bbe4
David Rgz <drs.rvnlrd@gmail.com>
2025-02-19 11:05:08 +0100

    fix(environment):added environment files

---
+++ src/app/environment/environment.dev.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  baseUrl: 'https://gateway.marvel.com/v1/public/',
+  publicKey: 'fe7b203a7b6179e6b5c380f35168c4f1',
+  privateKey: 'b573bb093145a6dd1fbf1f3cba02e299434b6369',
+};
